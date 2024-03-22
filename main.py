a433ae8451a856d48ab96f4af87c81c08a7f508a
Anastasia_Gryzunova <git@github.com:Anastasia-Gryzunova/Floyd.git>
2024-03-22 18:11:04 +0300

    Возможность делать циклы в графе (неориентированный и невзвешанный)

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ import random
 import math
 
 font_name = pygame.font.match_font('arial')
+
 def draw_text(surf, text, size, x, y):
-    WHITE = (255,163,251)
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
-
+    pass
 
 # Установка размеров окна
 WIDTH, HEIGHT = 800, 600
@@ -35,16 +29,13 @@ imge = pygame.image.load('vertex.png')
 imge.set_colorkey((255, 255, 255))
 img = pygame.transform.scale(imge, (40, 40))
 
-# Цвета
-WHITE = (230, 230, 250)
-BLACK = (0, 0, 0)
-
 # Основной цикл игры
 running = True
 flag = False
 vertices = []  # Список вершин
 lines = []
-chisla = []
+vertex_clicked = False
+start_pos = (0, 0)
 
 while running:
     for event in pygame.event.get():
@@ -55,17 +46,29 @@ while running:
                 running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Обработка нажатия левой кнопки мыши
             x, y = event.pos
-            vertices.append((x-15, y-15))  # Добавление новой вершины при клике мышкой
-            lines.append((x, y))
-            chisla.append(random.randint(1, 50))
+            vertex_clicked = False
+            for vertex in vertices:
+                if math.sqrt((vertex[0]-x)**2 + (vertex[1]-y)**2) < 50:  # Расстояние для проверки клика
+                    x, y = vertex  # Используем существующую точку
+                    lines.append((start_pos, (x+15, y+15)))
+                    vertex_clicked = True
+                    break
+
+            if not vertex_clicked:  # Добавляем новую вершину, только если точка не была найдена
+                vertices.append((x-15, y-15))  # Добавление новой вершины при клике мышкой
+            if start_pos != (0, 0) and not vertex_clicked:  # Если начальная точка уже установлена и мы не кликнули на существующую, добавляем линию
+                lines.append((start_pos, (x, y)))
+            if vertex_clicked:
+                start_pos = (x+15, y+15)
+            else:
+                start_pos = (x, y)
 
     # Отрисовка
     screen.fill(WHITE)
-    for vertex in range(1, len(vertices)+1):
-        screen.blit(img, vertices[vertex-1])
-        if  vertex != len(vertices):
-            pygame.draw.line(screen, BLACK, lines[vertex-1],  lines[vertex], 5)
-            draw_text(screen, str(chisla[vertex-1]), 30, ((vertices[vertex-1][0]+vertices[vertex][0])//2), ((vertices[vertex-1][1]+vertices[vertex][1])//2))
+    for vertex in vertices:
+        screen.blit(img, vertex)
+    for line in lines:
+        pygame.draw.line(screen, BLACK, line[0], line[1], 5)
     pygame.display.flip()
 
 pygame.quit()
